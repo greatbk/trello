a31232b0df617a137f5c7dd9fe2346587d7c8044
gangil <greatbk@daum.net>
2023-07-08 20:40:14 +0900

    .

--- src/store/cardEntities/actions.ts
+++ src/store/cardEntities/actions.ts
@@ -1,1 +1,12 @@
 import type * as T from './types'
+import {Card, UUID} from '../commonTypes'
+
+export const addCard = (payload: Card): T.AddCardAction => ({
+  type: '@cardEntities/add',
+  payload
+})
+
+export const removeCard = (payload: UUID): T.RemoveCardAction => ({
+  type: '@cardEntities/remove',
+  payload
+})
