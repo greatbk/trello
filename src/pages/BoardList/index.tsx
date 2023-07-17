ef319ac0b65b3f93bdb95b4414d9df23af99c108
gangil <greatbk@daum.net>
2023-07-17 19:28:09 +0900

    .

--- src/pages/BoardList/index.tsx
+++ src/pages/BoardList/index.tsx
@@ -3,6 +3,7 @@ import {FC, useMemo} from 'react'
 import {Icon} from '../../theme/daisyui'
 import {useCards} from '../../store/useCards'
 import ListCard from '../ListCard'
+import {Div} from '../../components'
 
 export type BoardListProps = {
   list: List
@@ -21,27 +22,26 @@ const BoardList: FC<BoardListProps> = ({list, onRemoveList, ...props}) => {
   )
 
   return (
-    <div {...props} className="p-2 m-2 border border-gray-300 rounded-lg">
+    <Div
+      {...props}
+      className="p-2 m-2 border border-gray-300 rounded-lg"
+      minWidth="13rem">
       <div className="flex justify-between mb-2">
         <p className="w-32 text-sm font-bold underline line-clamp-1">{list.title}</p>
-        <div className="flex justify-between ml-2">
-          <Icon name="remove" className="btn-error btn-xs" onClick={onRemoveList} />
-          <div className="flex">
-            <Icon
-              name="post_add"
-              className="btn-success btn-xs"
-              onClick={onPrependCard}
-            />
-            <Icon
-              name="playlist_add"
-              className="ml-2 btn-success btn-xs"
-              onClick={onAppendCard}
-            />
-          </div>
-        </div>
-        <div className="flex flex-col p-2">{children}</div>
       </div>
-    </div>
+      <div className="flex justify-between ml-2">
+        <Icon name="remove" className="btn-error btn-xs" onClick={onRemoveList} />
+        <div className="flex">
+          <Icon name="post_add" className="btn-success btn-xs" onClick={onPrependCard} />
+          <Icon
+            name="playlist_add"
+            className="ml-2 btn-success btn-xs"
+            onClick={onAppendCard}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col p-2">{children}</div>
+    </Div>
   )
 }
 
